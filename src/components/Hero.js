98aa8366f3dc11d2c8b5290152825ce19f008d87
Kapil <kapilgrv@gmail.com>
2025-10-06 01:00:12 +0530

    mobile view fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -11,7 +11,7 @@ const Hero = () => {
                     {/* <div className="overlay"></div> */}
                     <div className="container-fluid">
                         <div className="row">
-                            <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text" style={{ top: '10rem', left: '10rem' }}>
+                            <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text" style={{ top: '10rem', left: '5rem' }}>
                                 <div className="slider-text-inner js-fullheight">
                                     <div className="desc">
                                         <h1>Hey there !</h1>
